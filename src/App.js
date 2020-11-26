469ffd2d8181e7866e051653ebc6ccc7b7f0d5df
HongMuchan <muchang0824@gmail.com>
2020-11-27 00:38:33 +0900

    start-pack

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/Button";
+import Result from "./components/Result";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <React.Fragment>
+            <div className="result">
+                <p>app</p>
+            </div>
+            <Button text={"jdjd"}/>
+            <Result/>
+        </React.Fragment>
+    );
 }
 
 export default App;
